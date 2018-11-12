e39495ce3722ce05db22506b730cc1bef3f0692b
Terri Bedore <terribedore@gmail.com>
2018-11-12 13:08:49 -0600

    added service worker function.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+/* Service Worker.
+ * code from https://developers.google.com/web/fundamentals/primers/service-workers/#register_a_service_worker
+ */
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 let restaurants,
   neighborhoods,
   cuisines
